7a2c42ed44f1b419a289b1a0538d0b15f0854a28
DAZABAMUKER <wookis112@gmail.com>
2023-12-28 20:47:16 +0900

    앱 사용법 추가 앱 처음 실행 감지 AppStorage 설정

---
+++ NeotubeKaraoke/ManualView.swift
@@ -0,0 +1,18 @@
+//
+//  ManualView.swift
+//  NeotubeKaraoke
+//
+//  Created by 안병욱 on 12/25/23.
+//
+
+import SwiftUI
+
+struct ManualView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManualView()
+}
