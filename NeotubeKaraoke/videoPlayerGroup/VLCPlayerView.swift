663033d4b2d4aa80002f75f61e3315807fe23eb0
DAZABAMUKER <wookis112@gmail.com>
2024-11-17 01:59:32 +0900

    VP9 재생 실마리인 VLCKit을 추가함 로딩도 진짜 빠르고 VP9까지 재생된다지!!!!!

---
+++ NeotubeKaraoke/videoPlayerGroup/VLCPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VLCPlayerView.swift
+//  NeotubeKaraoke
+//
+//  Created by 안병욱 on 11/16/24.
+//
+
+import SwiftUI
+
+struct VLCPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VLCPlayerView()
+}
