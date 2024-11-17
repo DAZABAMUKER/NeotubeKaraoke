3fefa92c7248391b4c781a08b20cc8be8215d637
DAZABAMUKER <wookis112@gmail.com>
2024-11-17 20:08:51 +0900

    싱크 맞추기 까지 진행.

---
+++ NeotubeKaraoke/videoPlayerGroup/VLCView.swift
@@ -0,0 +1,18 @@
+//
+//  VLCView.swift
+//  NeotubeKaraoke
+//
+//  Created by 안병욱 on 11/17/24.
+//
+
+import SwiftUI
+
+struct VLCView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VLCView()
+}
---
+++ NeotubeKaraoke/videoPlayerGroup/vlcPlayerController.swift
@@ -0,0 +1,18 @@
+//
+//  vlcPlayerController.swift
+//  NeotubeKaraoke
+//
+//  Created by 안병욱 on 11/17/24.
+//
+
+import SwiftUI
+
+struct vlcPlayerController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    vlcPlayerController()
+}
