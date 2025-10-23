8f78c58eaa97d94c273796aa667a40251cc8d9ac
DAZABAMUKER <wookis112@gmail.com>
2025-10-24 07:22:01 +0900

    251024

---
+++ NeotubeKaraoke/LiquidGlassTestView.swift
@@ -0,0 +1,18 @@
+//
+//  LiquidGlassTestView.swift
+//  NeotubeKaraoke
+//
+//  Created by 안병욱 on 10/10/25.
+//
+
+import SwiftUI
+
+struct LiquidGlassTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiquidGlassTestView()
+}
